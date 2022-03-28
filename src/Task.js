f5f0db2f2ef3dfdca6913b5b8916bbd4b8351b84
himanshu <35025815+himanshuvashist@users.noreply.github.com>
2022-03-28 06:01:02 +0530

    task two

--- src/Task.js
+++ src/Task.js
@@ -5,7 +5,12 @@ export default function Task() {
     <div className="p-6">
       <div className="bg-indigo-100 text-stone-900 font-sans border-indigo-300 border-4 rounded-md p-4">
         <h4 className="text-2xl">Task</h4>
-        <p> In task one you have render live input's value below it</p>
+        <p>
+          {" "}
+          There should be a <strong>input field</strong> and a{" "}
+          <strong>button </strong>. If we click on button after typing
+          something, we should be able to see that last input's value below it.
+        </p>
       </div>
     </div>
   );
