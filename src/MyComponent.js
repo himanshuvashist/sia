cef56010b70694c22f713f258f23011430cc2400
himanshu <35025815+himanshuvashist@users.noreply.github.com>
2022-03-28 07:22:41 +0530

    task_three_sol

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -6,13 +6,45 @@ export default function MyComponent() {
   const second = ["pink"];
   const third = [];
   // ==============================
-  const [firstState] = useState(["tomato", "onion", "potato"]);
+  const [firstState, setFirstState] = useState(["tomato", "onion", "potato"]);
   const [secondState] = useState(["pink"]);
   const [thirdState] = useState([]);
   return (
-    <div className="p-6">
-      ⚒️⚒️⚒️
-      <div>go inside the MyComponent.js file to edit me.🙃</div>
+    <div
+      className="p-6"
+      onClick={() => {
+        //this piece of code is to show that there is a diffrence between state and normal variables, if we update normal variable it does not reflected into the DOM, but if we update the state if does reflect the change into the DOM. because every time the state gets udpated , the component re-renders
+        first.pop();
+        const newFirstState = [...firstState];
+        newFirstState.pop();
+        console.log(first);
+        setFirstState(newFirstState);
+      }}
+    >
+      <div className="bg-slate-200 mb-2 rounded-md">
+        <p>using variables</p>
+        {first.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+        {second.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+        {third.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+      </div>
+      <div className="bg-slate-200 mb-2 rounded-md">
+        <p>using state</p>
+        {firstState.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+        {secondState.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+        {thirdState.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+      </div>
     </div>
   );
 }
