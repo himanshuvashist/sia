d9d192ed4dc84891a3026890310eae93f99d4f3d
himanshu <35025815+himanshuvashist@users.noreply.github.com>
2022-03-28 06:24:39 +0530

    task three add

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,6 +1,14 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
 
 export default function MyComponent() {
+  const first = ["tomato", "onion", "potato"];
+  const second = ["pink"];
+  const third = [];
+  // ==============================
+  const [firstState] = useState(["tomato", "onion", "potato"]);
+  const [secondState] = useState(["pink"]);
+  const [thirdState] = useState([]);
   return (
     <div className="p-6">
       ⚒️⚒️⚒️
--- src/Task.js
+++ src/Task.js
@@ -2,15 +2,27 @@ import React from "react";
 
 export default function Task() {
   return (
-    <div className="p-6">
+    <div className="p-6 text-left">
       <div className="bg-indigo-100 text-stone-900 font-sans border-indigo-300 border-4 rounded-md p-4">
         <h4 className="text-2xl">Task</h4>
-        <p>
-          {" "}
-          There should be a <strong>input field</strong> and a{" "}
-          <strong>button </strong>. If we click on button after typing
-          something, we should be able to see that last input's value below it.
-        </p>
+        <p>we have three arrays :</p>
+        <br />
+        <p>["tomato", "onion", "potato"]</p>
+        <p>["pink"]</p>
+        <p>[]</p>
+        <br />
+        <strong>
+          show all three array's item in DOM below, inside html tags
+        </strong>
+        <div className="text-left">
+          <li>first using the normal const defined in the MyComponent.js </li>
+          <li>then using the state variables</li>
+          <li>
+            {" "}
+            and at last tell if there is any diffrence between render it using
+            the normal variables and with states
+          </li>
+        </div>
       </div>
     </div>
   );
